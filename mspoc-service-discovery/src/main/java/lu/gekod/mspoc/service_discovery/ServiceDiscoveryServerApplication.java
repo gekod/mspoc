bc5f19cb712f07b58e0d6eaf318dffbcc599a95b
Helder Araujo Carneiro <helder.araujo.carneiro@gmail.com>
2019-05-01 12:24:25 +0200

    Add service discovery server

---
+++ mspoc-service-discovery/src/main/java/lu/gekod/mspoc/service_discovery/ServiceDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package lu.gekod.mspoc.service_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceDiscoveryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoveryServerApplication.class, args);
+    }
+}
