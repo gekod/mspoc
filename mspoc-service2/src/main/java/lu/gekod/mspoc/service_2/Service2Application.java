50059a146ab275999907f4bc2d0fcecaf75c9d8b
Helder Araujo Carneiro <helder.araujocarneiro@arhs-cube.com>
2019-05-14 18:04:31 +0200

    Add Service DIscovery client

---
+++ mspoc-service2/src/main/java/lu/gekod/mspoc/service_2/Service2Application.java
@@ -0,0 +1,13 @@
+package lu.gekod.mspoc.service_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class Service2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Service2Application.class, args);
+    }
+}
